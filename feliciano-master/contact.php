ce3cc75119610df08206e07ecd00f44c5abe34e6
shynayip <shynayip913@gmail.com>
2026-01-31 16:11:19 +0800

    change layout for contact us

--- feliciano-master/contact.php
+++ feliciano-master/contact.php
@@ -82,22 +82,27 @@
         </div>
       </div>
     </section>
-		
-		<section class="ftco-section ftco-no-pt ftco-no-pb contact-section">
+      
+<section class="ftco-section ftco-no-pt ftco-no-pb contact-section">
 			<div class="container">
 				<div class="row d-flex align-items-stretch no-gutters">
 					<div class="col-md-6 pt-5 px-2 pb-2 p-md-5 order-md-last">
 						<h2 class="h4 mb-2 mb-md-5 font-weight-bold">Contact Us</h2>
 						<form action="#">
               <div class="form-group">
-	            <p><span><bold>Address:</bold></span> UTM Hotel & Residence
-                      NO 8 JALAN MAKTAB KAMPUNG DATUK KERAMAT, 54100 Kuala Lumpur, Malaysia</p>   
-                  </div>
-              <div class="form-group">
-	            <p><span><bold>Phone:</bold></span> <a href="tel://1234567920">+6012-26828864</a></p>
+                <input type="text" class="form-control" placeholder="Your Name">
               </div>
               <div class="form-group">
-              <p><span><bold>Email:</bold></span> <a href="mailto:info@yoursite.com">yobyong24@gmail.com</a></p>
+                <input type="text" class="form-control" placeholder="Your Email">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Subject">
+              </div>
+              <div class="form-group">
+                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+              </div>
+              <div class="form-group">
+                <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
               </div>
             </form>
 					</div>
@@ -108,7 +113,36 @@
 				</div>
 			</div>
 		</section>
-		
+		<section class="ftco-section contact-section">
+      <div class="container">
+        <div class="row d-flex contact-info">
+          <div class="col-md-12 mb-4">
+            <h2 class="h4 font-weight-bold">Contact Information</h2>
+          </div>
+          <div class="w-100"></div>
+          <div class="col-md-3 d-flex">
+          	<div class="dbox">
+	            <p><span>Address:</span> Kampung Datuk Keramat, 54000 Kuala Lumpur, Federal Territory of Kuala Lumpur</p>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex">
+          	<div class="dbox">
+	            <p><span>Phone:</span> <a href="tel://1234567920">+6012-26828864</a></p>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex">
+          	<div class="dbox">
+	            <p><span>Email:</span> <a href="mailto:info@yoursite.com">yobyong24@gmail.com</a></p>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex">
+          	<div class="dbox">
+	            <p><span>Website</span> <a href="#">yoursite.com</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 		
     <footer class="ftco-footer ftco-bg-dark ftco-section">
       <div class="container">
