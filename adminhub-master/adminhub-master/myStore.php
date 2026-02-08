6fad65a32d13fbff5fab5d6ba8b9e99c0507180d
wanhaziq67 <wanhaziq2762@gmail.com>
2026-02-08 20:04:28 +0800

    chage password for admin and staff

--- adminhub-master/adminhub-master/myStore.php
+++ adminhub-master/adminhub-master/myStore.php
@@ -61,7 +61,7 @@ include 'db.php';
       </a>
     </li>
     <li>
-      <a href="#" class="logout">
+      <a href="logout.php" class="logout">
         <i class='bx bxs-log-out-circle'></i>
         <span class="text">Logout</span>
       </a>
